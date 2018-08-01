d74732a1a66685c582babab9f3fb1c48ff7ce2d3
Johnny Soong <johnnysoong@hotmail.com>
2018-08-01 02:24:30 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(collection)
+  arr = []
+  i = 0
+  while i < collection.length
+  yield(arr.push(collection[i]))
+  i += 1
+  end
+  arr
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collection(array) do |name|
+  name.split(" ").first
+end
